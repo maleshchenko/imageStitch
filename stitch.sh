1efe97c4438f6c3f04c623cba2dab7c586c25ac2
Mykola Aleshchenko <nick130586@gmail.com>
2020-02-09 23:22:55 +0200

    The script now works in a cycle to add labels and uses a larger font

--- stitch.sh
+++ stitch.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-convert -gravity center 1.png label:"Before" +swap -append label:" " 1.png
-convert -gravity center 2.png label:"After" +swap -append label:" " 2.png
+for i in *.png; do
+    [ -f "$i" ] || break
+    filename="${i%.*}"
+    label=""
+
+    if [ "$filename" = "1" ]; then
+        label="Before"
+    else
+        label="After"
+    fi
+
+    convert -gravity center $i -font Arial -pointsize 40 label:"$label" +swap -append label:" " $i
+done
+
 convert 1.png 2.png +append output.png
